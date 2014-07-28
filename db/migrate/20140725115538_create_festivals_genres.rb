11bec51b76714e8f7cd1df30868b1b92bfb5c6fb
Cecile Veneziani <zora@patapouf.org>
2014-07-28 18:06:10 +0200

    Remove useless timestamps from habtm connection table

--- db/migrate/20140725115538_create_festivals_genres.rb
+++ db/migrate/20140725115538_create_festivals_genres.rb
@@ -3,8 +3,6 @@ class CreateFestivalsGenres < ActiveRecord::Migration
     create_table :festivals_genres do |t|
       t.belongs_to :festival
       t.belongs_to :genre
-
-      t.timestamps
     end
   end
 end
